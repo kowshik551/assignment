b00504d6e8d48008aab32e949d6f87e57b81ddf8
kowshik551 <reddykowshik551@gmail.com>
2024-11-21 17:58:50 +0530

    Daily Work Report

---
+++ 21-11-2024.py
@@ -0,0 +1,24 @@
+'''
+Django views are Python functions that take http requests and return http response, like HTML documents. A web page that uses Django is full of views with different tasks and missions. Views are usually put in a file called views.py located on your app's folder.
+
+
+Django, a popular Python web framework, simplifies the process of building web applications by providing robust tools and functionalities. One fundamental aspect of Django is the models.py file, which defines the structure of the application's database.
+
+In many ways, urls.py is the entry point for your project. It is usually the first file I open when I study a Django project. It is like reading a map before exploring a terrain. Essentially, urls.py contains the root URL configuration or URLConf of the entire project.
+    
+What is the purpose of settings.py? The settings.py file is typically used to store configuration information in Django. It may contain the site URL, paths to various directories, and other values that the executable code may use. By changing and supplementing this file, the developers configure Django projects
+
+The admin.py file is used to display your models in the Django admin panel. You can also customize your admin panel.
+
+Migrations are Django's way of propagating changes you make to your models (adding a field, deleting a model, etc.) into your database schema.
+
+The settings.py file is typically used to store configuration information in Django. It may contain the site URL, paths to various directories, and other values that the executable code may use. By changing and supplementing this file, the developers configure Django projects.
+
+Django, a high-level web framework for Python, includes built-in support for using SQLite as a database backend. SQLite is a lightweight, serverless database engine that stores data in a local file
+    
+uses of settings.py
+The settings.py file is typically used to store configuration information in Django. It may contain the site URL, paths to various directories, and other values that the executable code may use. By changing and supplementing this file, the developers configure Django projects
+
+'''
+
+
